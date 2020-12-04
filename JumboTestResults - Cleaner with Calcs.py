a47f71ef549255c7f0b21bbad5d783ef3c48e89a
adam-fowler1407 <74655223+adam-fowler1407@users.noreply.github.com>
2020-12-04 14:00:25 +0000

    Update JumboTestResults - Cleaner with Calcs.py

--- JumboTestResults - Cleaner with Calcs.py
+++ JumboTestResults - Cleaner with Calcs.py
@@ -1,18 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  5 12:45:24 2020
-
-@author: gbadafow
-"""
-
-# main cleaning Python program - objective open Excel Jumbo test reports, remove columns not needed, add some calculations and save to CVS file for analysis
 
 import pandas as pd
 import numpy as np
-
+from datetime import datetime as dt
 dfexcel = pd.read_excel(r'M:\Kemsley Paper Mill\Technical\Technical\PM4\PM4 Daily Analysis\Jumbo Test Results\Jumbo test results - All Data.xls') ##importing Excel file
 
-df = dfexcel.drop(['M/C','Ash','Black Spot 200 mm2','Blister','Boat Jumbo 30min Y N','Boat QA','Boat QC','Boat QC uncured','Boat Off Winder Uncured','Boat Winder Uncured Set 2',
+df = dfexcel.drop(['M/C','Ash','Black Spot 200 mm2','Blister','Boat Winder Uncured Set 1','Boat Jumbo 30min Y N','Boat QA','Boat QC','Boat QC uncured','Boat Off Winder Uncured','Boat Winder Uncured Set 2',
                    'Bright (457B) ~','Bright (457B) Lablink','Bright 457B QC','Bright UV Ex ~','Bright UV Ex Lablink','Bright UV Ex QC','Burst factor QA','Burst QA',
                    'Cobb 120m TS QA','Cobb 1m BS cured QA','Cobb 1m BS uncured QA',
                    'Cobb 1m TS uncured QA','Cobb 1m TS cured QA','Cobb 30m TS QA','Cobb 30min TS QC','Cobb 3m BS HOT QC','Cobb 5m BS QC','Cobb 5m TS QC','Density','DW VAR MD',
@@ -31,11 +23,17 @@ df['Turned Up'] = df['Turned Up'].astype(str)
 df[['Date','Time']] = df['Turned Up'].str.split(" ", expand=True)
 df[['y','m','d']] = df['Date'].str.split("-", expand=True)
 
-df['Month/Year'] = df['m']+"/"+df['y'] 
+df['Month/Year'] = df['m']+"/"+df['y']
+
+df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
+df['week'] = df['Date'].dt.isocalendar().week
 
 df['SCT Index'] = (df['SCT CD ~'] /df['Gsm QC ~'])*1000
 df['Burst Index'] = (df['Burst~']/df['Gsm QC ~'])
 df['CMT0 Index'] = (df['CMT0 QC'] /df['Gsm QC ~'])
+
+df = df.drop(df["SCT Index"].loc[df["SCT Index"]>200].index) # removes calculation errors where Index is greater than 200
+df = df.drop(df["CMT0 Index"].loc[df["CMT0 Index"]<2].index)
 
 df['SCT Retest'] = np.where(df['SCT CD QC'] == 100, 0, 1)
 df['Burst Retest'] = np.where(df['Burst Average'] == 100, 0, 1)
